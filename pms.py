d1b6d3c5a3aab66483ac6a59b28e8529c5177c29
Mattjs1089 <Mattjs89@gmail.com>
2020-06-24 16:41:48 -0400

    added functionality

--- pms.py
+++ pms.py
@@ -1,74 +1,75 @@
-#A python program to mimic the selections of a real-world
-#property management system(pms), found in hotels and residential buildings
+#A python program to mimic the functionality of a real-world
+#property management system(PMS), found in hotels and residential buildings
 import time
+import datetime
 import csv
 from csv import writer
 import random
 
-#print("ASCII ART")
+
+#Display main menu to the user
 print("\n:::::Python Property Management System:::::\n")
 time.sleep(1)
 print("Main Menu:")
 print("----------------\n")
 print("Property Code: PGH620\n")
-print("1. Search by Name\n" "2. Search by Confirmation #\n" "3. Search by Room #\n" "4. View In-House Guests\n" "5. View Depatures\n" "6. Make a Reservation\n" "7. Cancel Reservation\n")
-time.sleep(1)
+print("1. Search by Name\n" "2. Search by Confirmation #\n" "3. Search by Room #\n" "4. View In-House Guests\n" "5. View Departures\n" "6. Make a Reservation\n" "7. Cancel Reservation\n" "8. Log-off/Switch User\n")
+time.sleep(1) #mimics loading of the system
 menuSelect = input("Make a Selection: ")
 
-if menuSelect == "1":
+if menuSelect == "1": #DONE
     nameEntry = input("Please enter Last name: ")
+    #opens csv file containing guest records
     with open ('hotelguests.csv', 'rt') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        for column in csv_reader:
-            if column[0] == nameEntry:
-                print(column)
-        else:
-            if column[0] != nameEntry:
-                print("No Guest Found")
+        #searches for name entry, displays line
+        for row in csv_reader:
+            if row[0] == nameEntry:
+                print(row)
+                #ADD ELIF FOR "GUEST NOT FOUND"
+        
 
-elif menuSelect == "2":
+elif menuSelect == "2": #DONE
     confEntry = input("Enter Confirmation Number: ")
     with open ('hotelguests.csv', 'rt') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for column in csv_reader:
-            if column[0] == confEntry and confEntry.isdigit():
-                print(row)
-        else:
-            print("No Confirmation Number Exists.")
+            if column[5] == confEntry and confEntry.isdigit():
+                print(column)
+                #ADD ELIF FOR "GUEST NOT FOUND"
 
-elif menuSelect == "3":
+elif menuSelect == "3": #DONE
     roomEntry = input("Enter Room Number: ")
     with open ('hotelguests.csv', 'rt') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for column in csv_reader:
-            if column[0] == roomEntry and roomEntry.isdigit():
-                print(row)
-            else:
-                if not column[0]:
-                    print("No Guest Found")
+            if column[2] == roomEntry and roomEntry.isdigit():
+                print(column)
+                #ADD ELIF FOR "GUEST NOT FOUND"
 
 
-elif menuSelect == "4":
+elif menuSelect == "4": #DONE
     with open("hotelguests.csv", "r") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for lines in csv_reader:
             print(lines)
 
-elif menuSelect == "5":
-    with open("hotelguests.csv", "r") as csv_file:
+elif menuSelect == "5": #DONE
+    depDate = input("Enter Departure Date: ")
+    with open ('hotelguests.csv', 'rt') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        for lines in csv_reader:
-            print(lines[0])
-            #currently shows last names all guests
-            #needs to show departures ONLY for today's date
+        for column in csv_reader:
+            if column[4] == depDate:
+                print(column)
+                #ADD ELIF FOR NO DEPARTURES FOUND
 
-elif menuSelect == "6":
+elif menuSelect == "6": #DONE
     lastName = input("Enter a Last Name: ")
     firstName = input("Enter a First Name: ")
     roomNumber = input("Assign Room Number: ")
     arrivalDate = input("Enter an Arrival Date: ")
     departureDate = input("Enter a Departure Date: ")
-    confNum = random.randint(23000,30000)
+    confNum = random.randint(150000,160000)
     enterRate = input("Enter Rate Amt: $")
     enterCC = input("Enter a valid Credit Card Number: ")
     enterEXP = input("Enter Expiration Date as mm/yy: ")
@@ -79,19 +80,51 @@ elif menuSelect == "6":
     time.sleep(3)
     print("Reservation Uploaded with Confirmation # " + str(confNum))
 
-elif menuSelect == "7":
+elif menuSelect == "7": #DONE
     print("CXL MENU")
     print("---------")
     print("1. Cancel by Last Name \n")
     print("2. Cancel by Confirmation #\n")
     cancelSelect = input("Select an option: ")
+    lines = list()
+    if cancelSelect == "1":
+        lastName = input("Enter last name: ")
+        with open('hotelguests.csv', 'r') as readFile:
+            reader = csv.reader(readFile)
+            for row in reader:
+                lines.append(row)
+                for field in row:
+                    if field == lastName:
+                        lines.remove(row)
+        with open('hotelguests.csv', 'w') as writeFile:
+            writer = csv.writer(writeFile)
+            writer.writerows(lines)
+            time.sleep(2)
+            print("Reservation removed from PMS.")
 
-else:
-    logoffAnsw = print("LOG OFF? Y/N?")
+    else: 
+        confNumber = input("Enter confirmation number: ")
+        if cancelSelect == "2":
+            with open('hotelguests.csv', 'r') as readFile:
+                reader = csv.reader(readFile)
+                for row in reader:
+                    lines.append(row)
+                    for field in row:
+                        if field == confNumber:
+                            lines.remove(row)
+
+        with open('hotelguests.csv', 'w') as writeFile:
+            writer = csv.writer(writeFile)
+            writer.writerows(lines)
+            time.sleep(2)
+            print("Reservation removed from PMS.")
+
+elif menuSelect == 8:
+    logoffAnsw = input("LOG OFF? Y/N?")
     if logoffAnsw == 'Y' or 'y':
         print("Logging Off...")
         time.sleep(3)
         print("Logged off at " + time)
+    elif logoffAnsw == 'N' or 'n':
+        print("Not finished")
 
-
-    
