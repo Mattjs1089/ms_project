74ba67c7c4f930410ecfb4171338d9e19f36a758
Mattjs1089 <Mattjs89@gmail.com>
2020-07-29 13:46:51 -0400

    first commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,18 @@
+#FizzBuzz 
+
 for fizzbuzz in range(100):
+    #if number is divisible by 3 or 5, with remainder 0
     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
         print("fizzbuzz")
         continue
+    #if number is divisible by 3 with remainder of 0
     elif fizzbuzz % 3 == 0:
         print("fizz")
         continue
+    #if number is divisible by 5 with remainder of 0
     elif fizzbuzz % 5 == 0:
         print("buzz")
         continue
+    #otherwise, print fizzbuzz
     print(fizzbuzz)
 	
